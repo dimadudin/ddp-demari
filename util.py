436a8ee57ce9b5efd596e2370063ff1d75f3407c
dimadudin <dmitriy.i.dudin@gmail.com>
2024-02-13 09:36:10 +0300

    feat: compute and plot current and carrier densities

--- util.py
+++ util.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def solve(dn, a, c, b, d):
+def sys_solve(dn, a, c, b, d):
     alpha = np.zeros(dn)
     beta = np.zeros(dn)
 
@@ -18,3 +18,6 @@ def solve(dn, a, c, b, d):
         y[i-1] = alpha[i-1] * y[i] + beta[i-1]
 
     return y
+
+def i_calc(y, dx):
+    return dx/3 * (y[0] + 4 * np.sum(y[1:-1:2]) + 2 * np.sum(y[2:-1:2]) + y[-1])
